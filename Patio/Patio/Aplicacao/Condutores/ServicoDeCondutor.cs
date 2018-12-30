e04ded4221393343859b7993fad1da6946f65866
Veranildo Veras <veranildo@hotmail.com>
2018-12-30 14:00:44 -0200

    Refactory: Adicionando e melhorando os comentários.

--- Patio/Patio/Aplicacao/Condutores/ServicoDeCondutor.cs
+++ Patio/Patio/Aplicacao/Condutores/ServicoDeCondutor.cs
@@ -3,6 +3,11 @@ using Patio.Dominio.Tickets;
 
 namespace Patio.Aplicacao.Condutores
 {
+    // OBS.: Essa camada poderia ser simplesmente uma camada CRUD tradicional, para fins didáticos resolvemos criar usando conceitos do DDD.
+    // Se a equipe fosse usar REST com certeza iriamos usar o pipeline de middleware do .NET Core para suprimir esta camada, como pode ser visto em: 
+    // https://docs.microsoft.com/pt-br/aspnet/core/fundamentals/middleware/index?view=aspnetcore-2.2
+    // Existe também a premissa segundo Martin Fowler: Por que devemos criar um modelo rico? A resposta foi, por que não devemos começar com um modelo rico?
+
     // Serviço destinado para uso do contexto de checkout
     public sealed class ServicoDeCondutor : IServicoDeCondutor
     {
